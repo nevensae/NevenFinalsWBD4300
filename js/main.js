84924fd35e359fc2a330a8a5203158032cea7b87
nevensae <neven.jo7@gmail.com>
2019-04-09 18:25:23 +0200

    missingSVG

--- js/main.js
+++ js/main.js
@@ -1,48 +1,138 @@
 
-// SLIDER?????
+
+  // API
+  // https://api.myjson.com/bins/155q14
+const xhr = new XMLHttpRequest();
+xhr.responseType = 'json';
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        for(let i=1; i<10; i++){
+          let tmp = document.querySelector("#gl-"+i);
+          if(tmp !== null) {
+          tmp.setAttribute("src", xhr.response.galleryImages[i]);
+          }
+        }
+      }
+
+      if (xhr.status == 404) {
+        console.log("File or resource not found");
+      }
+    }
+};
+
+xhr.open('get', 'https://api.myjson.com/bins/12wq4w', true);
+xhr.send();
 
 
-function openModal() {
-    document.getElementById('myModal').style.display = "block";
-  }
-  
-  function closeModal() {
-    document.getElementById('myModal').style.display = "none";
-  }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    
-    if (n > slides.length) {
-    slideIndex = 1
+  // MAIN SLIDER
+
+  let sliderImages = document.querySelectorAll(".slide");
+  let arrowLeft = document.querySelector("#arrow-left");
+  let arrowRight = document.querySelector("#arrow-right");
+  let playPouse = document.querySelector("#play-pouse");
+  let current = 0;
+  let timer = null;
+  let play = 0;
+
+  // clear images
+
+  function reset() {
+    for(let i = 0; i < sliderImages.length; i++){
+      sliderImages[i].style.display = "none";
     }
-    
-    if (n < 1) {
-    slideIndex = slides.length
-    }
-    
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
   }
+
+  function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+  }
+  
+  // Show previous
+  function slideLeft(){
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+  }
+
+// Show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+  // left arrow click
+  if(arrowLeft !== null){
+  arrowLeft.addEventListener('click', function(){
+    if(current === 0){
+      current = sliderImages.length;
+    }
+    slideLeft();
+
+  });
+  }
+  if(arrowRight !== null){
+    // left arrow click
+    arrowRight.addEventListener('click', function(){
+      if(current === sliderImages.length - 1){
+        current = -1;
+      }
+      slideRight();
+  
+    });
+  
+   startSlide();
+  }
+  //  play pause
+
+  function setTimer(){
+    if (timer) {
+       // stop 
+       clearInterval( timer );
+       timer=null;
+    }
+    else {
+       timer = setInterval(function(){
+         arrowRight.click();
+       },2000);
+    }
+}
+if(playPouse !== null){
+playPouse.addEventListener('click', function(){
+  if(play === 0){
+    setTimer();
+    play = 1;
+    playPouse.innerHTML='<i class="fas fa-pause-circle"></i>';
+  }else{
+    setTimer();
+    play = 0;
+    playPouse.innerHTML= '<i class="fas fa-play-circle">';
+  }
+
+});
+}
+  
+
+// CFORM
+
+function validateForm()
+{
+  event.preventDefault();
+  var a=document.forms["loginform"]["email"].value;
+  var b=document.forms["loginform"]["subject"].value;
+  var c=document.forms["loginform"]["yourMessage"].value;
+    if (a === "") {
+      alert("Please enter your email");
+      return false;
+    }
+    if (b === "") {
+      alert("Please enter the subject");
+      return false;
+    }
+    if (c === "") {
+      alert("Please enter your message");
+      return false;
+    }
+}
